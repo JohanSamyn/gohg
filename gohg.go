2ee4a1a5d629b7762ae24eefc01f2b2876b4ec96
Johan Samyn <johan.samyn@gmail.com>
2012-08-22 21:11:54 +0200

    added gohg.go

---
+++ gohg.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2012, Johan P. P. Samyn <johan.samyn@gmail.com> All rights reserved.
+// Use of this source code is governed by the BSD-2-clause license
+// that can be found in the LICENSE.txt file.
+
+// Package gohg is a Go client library for using the Mercurial dvcs
+// using it's Command Server for better performance.
+//
+// For Mercurial see: http://mercurial/selenic.com/wiki.
+// For the Hg Command Server see: http://mercurial.selenic.com/wiki/CommandServer.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello from gohg!")
+}
