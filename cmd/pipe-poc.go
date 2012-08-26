d8a6a324bf5fad2989fc90c7b16a52f2eb7f401c
Johan Samyn <johan.samyn@gmail.com>
2012-08-26 15:06:31 +0200

    trying to master all input from Hg
    --HG--
    branch : poc

--- cmd/pipe-poc.go
+++ cmd/pipe-poc.go
@@ -21,7 +21,7 @@ func main() {
 	cmd := exec.Command("M:\\DEV\\hg-stable\\hg",
 		"-R", "C:\\DEV\\go\\src\\golout\\",
 		"--config", "ui.interactive=True",
-		"--config", "color=False",
+		"--config", "extensions.color=False",
 		"serve", "--cmdserver", "pipe")
 
 	pout, err := cmd.StdoutPipe()
@@ -41,7 +41,7 @@ func main() {
 	// 4101:	1 byte for the channel
 	//			4 bytes for the message length
 	//			up to 4096 bytes of data
-	s := make([]byte, 4101)
+	s := make([]byte, 1+4+8096)
 
 	// change this, so we can read only 5 bytes first, and so determine the
 	// channel and the length of the data. If channel = I or L, Hg is asking
@@ -52,90 +52,69 @@ func main() {
 	if err != io.EOF && err != nil {
 		log.Fatal("[5] ", i, err)
 	}
-	fmt.Printf("s=%v\n", s[0:100])
-	fmt.Printf("s=[[%s]]\n", s[0:100])
+	// fmt.Printf("s=%v\n", s[0:100])
+	// fmt.Printf("s=[[%s]]\n", s[0:100])
 
-	var ui uint32
+	var l uint32
 	buf := bytes.NewBuffer(s[1:5])
-	err = binary.Read(buf, binary.BigEndian, &ui)
+	err = binary.Read(buf, binary.BigEndian, &l)
 	if err != nil {
 		fmt.Println("binary.Read failed:", err)
 	}
-	fmt.Printf("buf=%v\n", *buf)
+	// fmt.Printf("buf=%v\n", *buf)
+
+	hgm0 := new(hgMsg)
+	hgc0 := new(hgCmd)
 
 	hgm := new(hgMsg)
 	hgm.Ch = string(s[0])
-	hgm.Ln = uint(ui)
+	hgm.Ln = uint(l)
 	hgm.Data = string(s[5 : 5+hgm.Ln])
-	fmt.Printf("hgMsg={\nChannel=%s\nLength=%v\nData=%s\n}\n",
-		hgm.Ch, hgm.Ln, hgm.Data)
+
+	hgm0.Data = strings.Replace(hgm.Data, "\n", "\\n", -1)
+	fmt.Printf("hgMsg={Channel: \"%s\"    Length: %v\n       Data: \"%s\"}\n",
+		hgm.Ch, hgm.Ln, hgm0.Data)
 
 	// ========== Sending a message to the Hg CS ==========
 
-	msg := new(hgMsg)
-	msg.Ch = "runcommand\n"
-	msg.Data = "summary"
-	msg.Ln = uint(len(msg.Data))
+	msg := new(hgCmd)
+	msg.Cmd = "runcommand\n"
+	msg.Args = "log\x00-l\x003"
+	msg.Ln = uint(len(msg.Args))
+	hgc0.Cmd = strings.Replace(msg.Cmd, "\n", "\\n", -1)
+	hgc0.Args = strings.Replace(msg.Args, "\n", "\\n", -1)
+	fmt.Printf("hgCmd={Command: \"%s\"    Length: %v\n       Args: \"%s\"}\n",
+		hgc0.Cmd, msg.Ln, hgc0.Args)
 
-	l1 := len(msg.Ch)
-	l3 := len(msg.Data)
+	l1 := len(msg.Cmd)
+	l3 := len(msg.Args)
 
 	sm := make([]byte, l1+4+l3)
-	copy(sm[0:l1], msg.Ch)
+	copy(sm[0:l1], msg.Cmd)
 	// fmt.Printf("l1=%d   l3=%d   ", l1, l3)
 	// fmt.Printf("len(sm)=%d   len(msg.Data)=%d\n", len(sm), len([]byte(msg.Data)))
-	copy(sm[l1+4:len(sm)], msg.Data)
-	fmt.Printf("sm=%v    len(sm)=%d\n", sm, len(sm))
-	fmt.Printf("sm=%s    len(sm)=%d\n", sm, len(sm))
+	copy(sm[l1+4:len(sm)], msg.Args)
+	// fmt.Printf("sm=%v    len(sm)=%d\n", sm, len(sm))
+	// fmt.Printf("sm=%s    len(sm)=%d\n", sm, len(sm))
 
-	/*
-		func Write(w io.Writer, order ByteOrder, data interface{}) error
-
-		Write writes the binary representation of data into w.
-		Data must be a fixed-size value or a slice of fixed-size values,
-		or a pointer to such data. Bytes written to w are encoded using
-		the specified byte order and read from successive fields of the data.
-
-		buf := new(bytes.Buffer)
-		var pi float64 = math.Pi
-		err := binary.Write(buf, binary.LittleEndian, pi)
-		if err != nil {
-		    fmt.Println("binary.Write failed:", err)
-		}
-		fmt.Printf("% x", buf.Bytes())
-	*/
-	// bs := make([]byte, 4)
 	wbuf := new(bytes.Buffer)
 	err = binary.Write(wbuf, binary.BigEndian, uint32(msg.Ln))
 	if err != nil {
 		fmt.Println("binary.Write failed:", err)
 	}
-	// How to get the content of wbuf into bs or msg.Ln ?
-	// bs = wbuf
-	// err = binary.Read(buf, binary.BigEndian, &bs)
-	// if err != nil {
-	// 	fmt.Println("binary.Read failed:", err)
-	// }
 
 	bf := make([]byte, 4)
 	// fmt.Printf("len(bf)=%d\n", len(bf))
-	var n int
-	n, err = io.ReadFull(wbuf, bf)
+	// var n int
+	_, err = io.ReadFull(wbuf, bf)
 	if err != nil {
 		fmt.Println("io.ReadFull failed:", err)
 	}
-	fmt.Printf("n=%d    bf=%v\n", n, bf)
-
-	// // func ReadUvarint(r io.ByteReader) (uint64, error)
-	// var uuii uint64
-	// uuii, err = binary.ReadUvarint(wbuf)
-	// if err != nil {
-	// 	fmt.Println("binary.ReadUvarint failed:", err)
-	// }
+	// fmt.Printf("n=%d    bf=%v\n", n, bf)
 
 	copy(sm[l1:l1+4], bf)
-	fmt.Printf("sm=%v\n", sm)
-	fmt.Printf("bf=%v   sm=[[%s]]    len(sm)=%d\nwbuf=%v\n", bf, sm, len(sm), *wbuf)
+	// fmt.Printf("sm=%v\n", sm)
+	// fmt.Printf("bf=%v   sm=[[%s]]    len(sm)=%d\nwbuf=%v\n", bf, sm, len(sm), *wbuf)
 	// copy(sm[l1-1:l1-1+4], bs)
 	// fmt.Printf("bs=%v   sm=[[%s]]   uuii=%v\nwbuf=%v\n", bs, sm, uuii, *wbuf)
 
@@ -171,16 +150,19 @@ func main() {
 	// var br []byte
 	var hgm2 hgMsg
 	for {
-		hgm2 = readFromHg(pout)
-		hgm2.Data = strings.Replace(hgm2.Data, "\n", "\\n", -1)
-		fmt.Printf("hgm2=%s\n", hgm2)
+		hgm2 = receiveFromHg(pout)
+		hgm0.Data = strings.Replace(hgm2.Data, "\n", "\\n", -1)
+		fmt.Printf("hgMsg={Channel: \"%s\"    Length: %v\n       Data: \"%s\"}\n",
+			hgm2.Ch, hgm2.Ln, hgm0.Data)
+		// hgm2.Data = strings.Replace(hgm2.Data, "\n", "\\n", -1)
+		// fmt.Printf("hgm2=%s\n", hgm2)
 		// fmt.Printf("hgm2=%v\nhgm2=%s", hgm2, hgm2)
 		// switch {
 		// 	case
 		// }
-		if hgm2.Ch == "r" {
-			break
-		}
+		// if hgm2.Ch == "r" {
+		// 	break
+		// }
 	}
 
 	// ========== Closing the connection ==========
@@ -199,11 +181,17 @@ type hgMsg struct {
 	Data string
 }
 
+type hgCmd struct {
+	Cmd  string
+	Ln   uint
+	Args string
+}
+
 // func decodeHgMsg(s string) (hgMsg, error) {
 // 	return "", nil
 // }
 
-func readFromHg(pout io.ReadCloser) hgMsg {
+func receiveFromHg(pout io.ReadCloser) hgMsg {
 	s := make([]byte, 4101)
 
 	// change this, so we can read only 5 bytes first, and so determine the
@@ -218,17 +206,21 @@ func readFromHg(pout io.ReadCloser) hgMsg {
 	// fmt.Printf("s=%v\n", s[0:100])
 	// fmt.Printf("s=[[%s]]\n", s[0:100])
 
-	var ui uint32
+	hgm := new(hgMsg)
+
+	if s[0] == byte("r"[0]) {
+		return *hgm
+	}
+	var l uint32
 	buf := bytes.NewBuffer(s[1:5])
-	err = binary.Read(buf, binary.BigEndian, &ui)
+	err = binary.Read(buf, binary.BigEndian, &l)
 	if err != nil {
 		fmt.Println("binary.Read failed:", err)
 	}
 	// fmt.Printf("buf=%v\n", *buf)
 
-	hgm := new(hgMsg)
 	hgm.Ch = string(s[0])
-	hgm.Ln = uint(ui)
+	hgm.Ln = uint(l)
 	hgm.Data = string(s[5 : 5+hgm.Ln])
 	// fmt.Printf("hgMsg={\nChannel=%s\nLength=%v\nData=%s\n}\n",
 	// 	hgm.Ch, hgm.Ln, hgm.Data)
