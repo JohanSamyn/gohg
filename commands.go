33e3c2a10f3925886e12b368934a803d4238f8fc
Johan Samyn <johan.samyn@gmail.com>
2012-09-04 17:48:24 +0200

    put commands in a separate sourcefile

---
+++ commands.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2012, Johan P. P. Samyn <johan.samyn@gmail.com> All rights reserved.
+// Use of this source code is governed by the Simplified BSD License
+// that can be found in the LICENSE.txt file.
+
+package gohg
+
+import (
+	"log"
+)
+
+func RunCommand() {
+	// place this function in a separate sourcefile
+	log.Println("RunCommand: not implemented yet")
+} // RunCommand()
