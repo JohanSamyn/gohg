dd7369060243b432efc87f45b37caafafd54a32f
Johan Samyn <johan.samyn@gmail.com>
2012-09-04 17:48:55 +0200

    utility script

---
+++ t.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+cd C:\DEV\go\src\gohg
+go build gohg.go
+choice /c yn /m "Continue?"
+if errorlevel == 255 goto end
+if errorlevel == 2 goto end
+if errorlevel == 1 goto test
+if errorlevel == 0 goto end
+
+:test
+cd C:\DEV\go\src\gohg\test
+go build test.go
+choice /c yn /m "Continue?"
+cls & test
+
+:end
