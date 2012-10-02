15b4ac0096bb1626f38aba39e03cef599d57e6fc
Johan Samyn <johan.samyn@gmail.com>
2012-10-02 22:28:19 +0200

    add a test for Version()

---
+++ gohg_lib/version_test.go
@@ -0,0 +1,21 @@
+// Copyright (C) 2012, The gohg Authors. All rights reserved.
+// Use of this source code is governed by a BSD style license
+// that can be found in the LICENSE.txt file.
+
+package gohg_lib_test
+
+import (
+	. "gohg/gohg_lib"
+	"testing"
+)
+
+func TestHgClient_Version(t *testing.T) {
+	ver, _, err := HgClient.Version()
+	if err != nil {
+		t.Fatal(err)
+	}
+	minimalversion := "1.9"
+	if ver < minimalversion {
+		t.Error("HgClient.Version(): expected value >= " + minimalversion + " but got " + ver)
+	}
+}
