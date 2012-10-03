9cc69ed34275efcf6ae75aee8efc586067c6e5a9
Johan Samyn <johan.samyn@gmail.com>
2012-10-03 21:57:47 +0200

    added summary.go

---
+++ gohg_lib/summary.go
@@ -0,0 +1,12 @@
+// Copyright (C) 2012, The gohg Authors. All rights reserved.
+// Use of this source code is governed by a BSD style license
+// that can be found in the LICENSE.txt file.
+
+package gohg_lib
+
+import ()
+
+// Add implements the 'hg summary' command.
+func (hgcl *hgclient) Summary(files []string) error {
+	return nil
+}
