103a0741b53ee0c973472f4812a7b25b67c741b5
Johan Samyn <johan.samyn@gmail.com>
2012-11-11 19:34:21 +0100

    corrected type in var-initializations

--- test/example1.go
+++ test/example1.go
@@ -16,10 +16,10 @@ func main() {
 	// Set var hgexe to whatever is appropriate for your situation.
 	// You can also change it to test with different versions of Mercurial.
 	// hgexe := "M:/DEV/hg-stable/hg"
-	hgexe = "hg"
+	hgexe := "hg"
 	// repo := "C:/DEV/thg-qt-work"
 	// repo = "C:/DEV/go/src/bitbucket.org/gohg/gohg"
-	repo = "."
+	repo := "."
 
 	fmt.Println("========== Begin of example1 ==========")
 
