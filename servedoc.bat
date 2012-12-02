1e8934e87a0ef6d3b17a1f99c0dd415c1d8ee4d0
Johan Samyn <johan.samyn@gmail.com>
2012-12-02 15:08:36 +0100

    servedoc.bat: modified GOPATH

--- servedoc.bat
+++ servedoc.bat
@@ -1,3 +1,4 @@
 @echo off
-set GOPATH=%GOPATH%;C:\DEV\go
-C:\Windows\System32\cmd.exe /c start "Godoc Server http://localhost:6061" "C:\Programs\Go\bin\godoc.exe" -http=localhost:6061 -goroot="C:\Programs\Go\." -tabwidth=4
+::set GOPATH=%GOPATH%;C:\DEV\go
+set GOPATH=C:\DEV\go
+C:\Windows\System32\cmd.exe /c start "Godoc Server http://localhost:6061" "C:\Programs\Go\bin\godoc.exe" -goroot="C:\Programs\Go\." -http=localhost:6061 -tabwidth=4
