1d9f0e39d04b3ba60d34e1bcd4b72fa43dd05e9e
Johan Samyn <johan.samyn@gmail.com>
2012-12-02 14:35:53 +0100

    log: added new test for for non-empty repo

--- gohg_lib/log_test.go
+++ gohg_lib/log_test.go
@@ -5,10 +5,12 @@
 package gohg_lib_test
 
 import (
+	"os"
+	"os/exec"
 	"testing"
 )
 
-func TestHgClient_Log_EmptyRepo(t *testing.T) {
+func TestHgClient_Log_NewRepo(t *testing.T) {
 	hct := setup(t)
 	defer teardown(t, hct)
 
@@ -22,7 +24,7 @@ func TestHgClient_Log_EmptyRepo(t *testing.T) {
 	}
 }
 
-func TestHgClient_Log_Tip(t *testing.T) {
+func TestHgClient_Log_Empty(t *testing.T) {
 	hct := setup(t)
 	defer teardown(t, hct)
 
@@ -33,5 +35,33 @@ func TestHgClient_Log_Tip(t *testing.T) {
 	}
 	if data != nil {
 		t.Fatal("Empty repo should have empty log")
+	}
+}
+
+func TestHgClient_Log_NotEmpty(t *testing.T) {
+	hct := setup(t)
+	defer teardown(t, hct)
+
+	// log should produce info for non-empty repo
+
+	// have to make the working dir dirty !
+	f, err := os.Create(hct.RepoRoot() + "/a")
+	_, _ = f.Write([]byte{'a', 'a', 'a'})
+	f.Sync()
+	f.Close()
+	// add all there is to add to the repo and commit
+	var cmd *exec.Cmd
+	cmd = exec.Command(hct.HgExe(), "--cwd", testdir, "commit", "-Am\"test commit\"")
+	if err = cmd.Run(); err != nil {
+		t.Fatal(err)
+	}
+
+	// now we can perform the real test
+	data, err := hct.Log([]string{"-r", "tip"})
+	if err != nil {
+		t.Error(err)
+	}
+	if data == nil {
+		t.Fatal("Non-empty repo should non-empty log")
 	}
 }
