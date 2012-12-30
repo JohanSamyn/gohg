e0f4fdfe98c1f2a9ee442656f37b23b62da4e554
Johan Samyn <johan.samyn@gmail.com>
2012-12-30 12:39:54 +0100

    servedoc: assure GOPATH is restored properly

--- servedoc.bat
+++ servedoc.bat
@@ -1,4 +1,5 @@
 @echo off
-::set GOPATH=%GOPATH%;C:\DEV\go
+setlocal
 set GOPATH=C:\DEV\go
 C:\Windows\System32\cmd.exe /c start "Godoc Server http://localhost:6161" "C:\Programs\Go\bin\godoc.exe" -goroot="C:\Programs\Go\." -http=localhost:6161 -tabwidth=4
+endlocal
