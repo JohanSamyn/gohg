b27264c2c9d4b799cd52c290aacd9d952610bda5
Johan Samyn <johan.samyn@gmail.com>
2012-12-30 10:06:43 +0100

    - delete previous coverage results before new coverage run - added some comment - added runtime feedback comments

--- cov.cmd
+++ cov.cmd
@@ -1,4 +1,18 @@
 @echo off
+::
+:: usage: cov <package> <logfile-prefix>
+:: examples:
+::		cov . gohg
+::		cov gohg gohg
+::
+echo === Deleting existing logfiles...
+if exist %2-coverage.json (del %2-coverage.json)
+if exist %2-coverage.log (del %2-coverage.log)
+if exist %2-coverage-annotate.log (del %2-coverage-annotate.log)
+echo === Gathering coverage info...
 gocov test %1 > %2-coverage.json
+echo === Creating logfile...
 gocov report %2-coverage.json > %2-coverage.log
+echo === Annotating functions...
 gocov annotate %2-coverage.json %2.* > %2-coverage-annotate.log
+echo === Done!
