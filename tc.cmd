f47e8d5e543634ad2a2c66beb3ee1d5412966d8a
Johan Samyn <johan.samyn@gmail.com>
2012-12-30 22:18:05 +0100

    changed to accomodate the call to Version() in client.go

--- tc.cmd
+++ tc.cmd
@@ -8,5 +8,12 @@ if "%1" == "all" (
   go test %2 %3 %4 %5 %6 %7 %8 %9
   goto end
 )
-go test %2 %3 %4 %5 %6 %7 %8 %9 client.go commands.go options.go util.go setup_and_teardown_test.go %1.go %1_test.go
+if "%1" == "version" (
+  go test %2 %3 %4 %5 %6 %7 %8 %9 client.go commands.go options.go util.go setup_and_teardown_test.go %1.go %1_test.go
+  goto end
+)
+:: Need version.go to avoid compile error, cause Version() is called in client.go.
+go test %2 %3 %4 %5 %6 %7 %8 %9 client.go commands.go options.go util.go version.go setup_and_teardown_test.go %1.go %1_test.go
+goto end
+
 :end
