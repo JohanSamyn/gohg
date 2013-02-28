fb45c93e265e6943ab379ce8f489b0608b1882de
Johan Samyn <johan.samyn@gmail.com>
2013-02-28 21:38:13 +0100

    added command heads

---
+++ heads.go
@@ -0,0 +1,49 @@
+// Copyright 2012, The gohg Authors. All rights reserved.
+// Use of this source code is governed by a BSD style license
+// that can be found in the LICENSE file.
+
+package gohg
+
+import (
+	"fmt"
+)
+
+type headsCmd struct {
+	Template
+	Debug
+	Profile
+	Traceback
+}
+
+func (cmd *headsCmd) String() string {
+	return fmt.Sprintf(
+		"headsCmd = {\n    debug: (%t) %t\n    traceback: (%T) %t\n    profile: (%T) %t\n}\n",
+		cmd.Debug, cmd.Debug, cmd.Traceback, cmd.Traceback, cmd.Profile, cmd.Profile)
+}
+
+// Heads provides the 'hg heads' command.
+func (hgcl *HgClient) Heads(opts ...optionAdder) ([]byte, error) {
+
+	// applies type defaults
+	cmd := new(headsCmd)
+
+	// apply gohg defaults
+	cmd.Debug = false
+	cmd.Profile = false
+	cmd.Traceback = false
+
+	hgcmd := []string{"heads"}
+
+	var err error
+
+	// apply option values given by the caller
+	for _, o := range opts {
+		err = o.addOption(cmd, &hgcmd)
+	}
+
+	fmt.Printf("hgcmd = %v", hgcmd)
+
+	var data []byte
+	data, err = hgcl.runcommand(&hgcmd)
+	return data, err
+}
---
+++ heads_test.go
@@ -0,0 +1,37 @@
+// Copyright 2012, The gohg Authors. All rights reserved.
+// Use of this source code is governed by a BSD style license
+// that can be found in the LICENSE file.
+
+package gohg
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestHgClient_Heads(t *testing.T) {
+	hct := setup(t)
+	defer teardown(t, hct)
+
+	var expected string = "\"0\n\""
+
+	f, err := os.Create(hct.RepoRoot() + "/a")
+	_, _ = f.Write([]byte{'a', 'a', 'a'})
+	f.Sync()
+	f.Close()
+
+	cmd := exec.Command(hct.HgExe(), "--cwd", hct.RepoRoot(), "ci", "-Am\"first commit\"")
+	if err := cmd.Run(); err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := hct.Heads(Template("{rev}\n"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(got) != expected {
+		t.Fatalf("Test Heads: expected:\n%s\n but got:\n%s\n", expected, got)
+	}
+}
