9ba7c2f26de21de9651fea6f12c9f4f7231a25d9
Johan Samyn <johan.samyn@gmail.com>
2013-03-10 17:12:37 +0100

    gt: added init.go for standard testruns

--- gt.bat
+++ gt.bat
@@ -13,8 +13,13 @@ if "%1" == "version" (
   go test %2 %3 %4 %5 %6 %7 %8 %9 client.go options.go add.go setup_and_teardown_test.go %1.go %1_test.go
   goto end
 )
+if "%1" == "init" (
+  go test %2 %3 %4 %5 %6 %7 %8 %9 client.go options.go add.go version.go setup_and_teardown_test.go %1.go %1_test.go
+  goto end
+)
 :: Need version.go to avoid compile error, cause Version() is called in client.go.
-go test %2 %3 %4 %5 %6 %7 %8 %9 client.go options.go add.go version.go setup_and_teardown_test.go %1.go %1_test.go
+:: Need init.go for tests
+go test %2 %3 %4 %5 %6 %7 %8 %9 client.go options.go init.go add.go version.go setup_and_teardown_test.go %1.go %1_test.go
 goto end
 
 :usage
