9a34a02cfa45dc3bf099c22d2ae39dd09f505c57
Johan Samyn <johan.samyn@gmail.com>
2013-03-25 22:51:48 +0100

    options: factored out some code + added some more options

--- options.go
+++ options.go
@@ -17,23 +17,35 @@ var errstr = "command %s has no option %s"
 
 type (
 	Active        bool   // -a --active
+	All           bool   //    --all
 	Closed        bool   // -c --closed
 	Cwd           string //    --cwd
 	Debug         bool   //    --debug
 	Destpath      string // no equivalent Hg option, used by Init()
+	Dryrun        bool   // -n --dry-run (<->ShowNum)
+	Exclude       string // -X --exclude
+	Git           bool   // -g --git
+	Include       string // -I --include
+	Insecure      bool   //    --insecure
 	Limit         int    // -l --limit
 	Mq            bool   //    --mq
+	Patch         bool   // -p --patch
 	Profile       bool   //    --profile
 	Quiet         bool   // -q --quiet
 	Remote        bool   //    --remote
+	RemoteCmd     string //    --remotecmd
 	Repository    string // -R --repository
 	Rev           string // -r --rev REV
 	ShowBookmarks bool   // -B --bookmarks
 	ShowBranch    bool   // -b --branch
 	ShowId        bool   // -i --id
-	ShowNum       bool   // -n --num
-	ShowTags      bool   // -t --tags
+	ShowNum       bool   // -n --num (<->Dryrun)
+	ShowTags      bool   // -t --tags (<->Topo)
+	Ssh           string // -e --ssh
+	Style         string //    --style
+	Subrepos      bool   // -S --subrepos
 	Template      string //    --template
+	Topo          bool   // -t --topo (<->Tags)
 	Traceback     bool   //    --traceback
 	Verbose       bool   // -v --verbose
 )
@@ -52,268 +64,172 @@ type optionAdder interface {
 // but these error messages are silently ignored for the moment.
 
 func (o Active) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Active")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--active")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Active")
-	}
-	return nil
+	return addBoolOpt(bool(o), "Active", "--active", i, hgcmd)
+}
+
+func (o All) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "All", "--all", i, hgcmd)
 }
 
 func (o Closed) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Closed")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--closed")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Closed")
-	}
-	return nil
+	return addBoolOpt(bool(o), "Closed", "--closed", i, hgcmd)
 }
 
 func (o Cwd) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Cwd")
-	if f.IsValid() && f.CanSet() {
-		f.SetString(string(o))
-		if string(o) != "" {
-			*hgcmd = append(*hgcmd, "--cwd")
-			*hgcmd = append(*hgcmd, string(o))
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Cwd")
-	}
-	return nil
+	return addStringOpt(string(o), "Cwd", "--cwd", i, hgcmd)
 }
 
 func (o Debug) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Debug")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--debug")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Debug")
-	}
-	return nil
+	return addBoolOpt(bool(o), "Debug", "--debug", i, hgcmd)
 }
 
 func (o Destpath) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Destpath")
-	if f.IsValid() && f.CanSet() {
-		f.SetString(string(o))
-		if string(o) != "" {
-			*hgcmd = append(*hgcmd, string(o))
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Destpath")
-	}
-	return nil
+	return addStringOpt(string(o), "Destpath", "", i, hgcmd)
+}
+
+func (o Dryrun) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "Dryrun", "--dry-run", i, hgcmd)
+}
+
+func (o Exclude) addOption(i interface{}, hgcmd *[]string) error {
+	return addStringOpt(string(o), "Exclude", "--exclude", i, hgcmd)
+}
+
+func (o Git) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "Git", "--git", i, hgcmd)
+}
+
+func (o Include) addOption(i interface{}, hgcmd *[]string) error {
+	return addStringOpt(string(o), "Include", "--include", i, hgcmd)
+}
+
+func (o Insecure) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "Insecure", "--insecure", i, hgcmd)
 }
 
 func (o Limit) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Limit")
-	if f.IsValid() && f.CanSet() {
-		f.SetInt(int64(o))
-		if int64(o) > 0 {
-			*hgcmd = append(*hgcmd, "-l")
-			*hgcmd = append(*hgcmd, strconv.Itoa(int(int64(o))))
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Limit")
-	}
-	return nil
+	return addIntOpt(int(o), "Limit", "--limit", i, hgcmd)
 }
 
 func (o Mq) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Mq")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--mq")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Mq")
-	}
-	return nil
+	return addBoolOpt(bool(o), "Mq", "--mq", i, hgcmd)
+}
+
+func (o Patch) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "Patch", "--patch", i, hgcmd)
 }
 
 func (o Profile) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Profile")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--profile")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Profile")
-	}
-	return nil
+	return addBoolOpt(bool(o), "Profile", "--profile", i, hgcmd)
 }
 
 func (o Quiet) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Quiet")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "-q")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Quiet")
-	}
-	return nil
+	return addBoolOpt(bool(o), "Quite", "-q", i, hgcmd)
 }
 
 func (o Remote) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Remote")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--remote")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Remote")
-	}
-	return nil
+	return addBoolOpt(bool(o), "Remote", "--remote", i, hgcmd)
+}
+
+func (o RemoteCmd) addOption(i interface{}, hgcmd *[]string) error {
+	return addStringOpt(string(o), "RemoteCmd", "--remotecmd", i, hgcmd)
 }
 
 func (o Repository) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Repository")
-	if f.IsValid() && f.CanSet() {
-		f.SetString(string(o))
-		if string(o) != "" {
-			*hgcmd = append(*hgcmd, "-R")
-			*hgcmd = append(*hgcmd, string(o))
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Repository")
-	}
-	return nil
+	return addStringOpt(string(o), "Repository", "-R", i, hgcmd)
 }
 
 func (o Rev) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Rev")
-	if f.IsValid() && f.CanSet() {
-		f.SetString(string(o))
-		if string(o) != "" {
-			*hgcmd = append(*hgcmd, "-r")
-			*hgcmd = append(*hgcmd, string(o))
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Rev")
-	}
-	return nil
+	return addStringOpt(string(o), "Rev", "--rev", i, hgcmd)
 }
 
 func (o ShowBookmarks) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("ShowBookmarks")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--bookmarks")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "ShowBookmarks")
-	}
-	return nil
+	return addBoolOpt(bool(o), "ShowBookmarks", "--bookmarks", i, hgcmd)
 }
 
 func (o ShowBranch) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("ShowBranch")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--branch")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "ShowBranch")
-	}
-	return nil
+	return addBoolOpt(bool(o), "ShowBranch", "--branch", i, hgcmd)
 }
 
 func (o ShowId) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("ShowId")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		// fmt.Printf("ShowId.transl %v\n", bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--id")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "ShowId")
-	}
-	return nil
+	return addBoolOpt(bool(o), "ShowId", "--id", i, hgcmd)
 }
 
 func (o ShowNum) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("ShowNum")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		// fmt.Printf("ShowNum.transl %v\n", bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--num")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "ShowNum")
-	}
-	return nil
+	return addBoolOpt(bool(o), "ShowNum", "--num", i, hgcmd)
 }
 
 func (o ShowTags) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("ShowTags")
-	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--tags")
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "ShowTags")
-	}
-	return nil
+	return addBoolOpt(bool(o), "ShowTags", "--tags", i, hgcmd)
+}
+
+func (o Ssh) addOption(i interface{}, hgcmd *[]string) error {
+	return addStringOpt(string(o), "Ssh", "--ssh", i, hgcmd)
+}
+
+func (o Style) addOption(i interface{}, hgcmd *[]string) error {
+	return addStringOpt(string(o), "Style", "--style", i, hgcmd)
+}
+
+func (o Subrepos) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "Subrepos", "--subrepos", i, hgcmd)
 }
 
 func (o Template) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Template")
-	if f.IsValid() && f.CanSet() {
-		f.SetString(string(o))
-		if string(o) != "" {
-			*hgcmd = append(*hgcmd, "--template")
-			*hgcmd = append(*hgcmd, string(o))
-		}
-	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Traceback")
-	}
-	return nil
+	return addStringOpt(string(o), "Template", "--template", i, hgcmd)
+}
+
+func (o Topo) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "Topo", "--topo", i, hgcmd)
 }
 
 func (o Traceback) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Traceback")
+	return addBoolOpt(bool(o), "Traceback", "--traceback", i, hgcmd)
+}
+
+func (o Verbose) addOption(i interface{}, hgcmd *[]string) error {
+	return addBoolOpt(bool(o), "Verbose", "-v", i, hgcmd)
+}
+
+func addBoolOpt(opt bool, optname string, cmd string, i interface{}, hgcmd *[]string) error {
+	f := reflect.ValueOf(i).Elem().FieldByName(optname)
 	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "--traceback")
+		f.SetBool(opt)
+		if opt {
+			*hgcmd = append(*hgcmd, cmd)
 		}
 	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Traceback")
+		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), optname)
 	}
 	return nil
 }
 
-func (o Verbose) addOption(i interface{}, hgcmd *[]string) error {
-	f := reflect.ValueOf(i).Elem().FieldByName("Verbose")
+func addIntOpt(opt int, optname string, cmd string, i interface{}, hgcmd *[]string) error {
+	f := reflect.ValueOf(i).Elem().FieldByName(optname)
 	if f.IsValid() && f.CanSet() {
-		f.SetBool(bool(o))
-		if bool(o) {
-			*hgcmd = append(*hgcmd, "-v")
+		f.SetInt(int64(opt))
+		if opt > 0 {
+			*hgcmd = append(*hgcmd, cmd)
+			*hgcmd = append(*hgcmd, strconv.Itoa(opt))
 		}
 	} else {
-		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), "Verbose")
+		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), optname)
+	}
+	return nil
+}
+
+func addStringOpt(opt string, optname string, cmd string, i interface{}, hgcmd *[]string) error {
+	f := reflect.ValueOf(i).Elem().FieldByName(optname)
+	if f.IsValid() && f.CanSet() {
+		f.SetString(opt)
+		if cmd != "" { // think about Destpath f.i.
+			*hgcmd = append(*hgcmd, cmd)
+		}
+		if opt != "" {
+			*hgcmd = append(*hgcmd, opt)
+		}
+	} else {
+		return fmt.Errorf(errstr, strings.Title((*hgcmd)[0]), optname)
 	}
 	return nil
 }
