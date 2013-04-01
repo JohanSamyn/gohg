08b651eefe8f0138d563ed7311ac3aa99076b053
Johan Samyn <johan.samyn@gmail.com>
2013-04-01 22:31:25 +0200

    options: add function name to err message

--- options.go
+++ options.go
@@ -360,7 +360,7 @@ func addBoolOpt(opt bool, optname string, cmd string, cmdOpts interface{}, hgcmd
 			*hgcmd = append(*hgcmd, cmd)
 		}
 	} else {
-		return fmt.Errorf("command %s has no option %s", strings.Title((*hgcmd)[0]), optname)
+		return fmt.Errorf("adBoolOpt(): command %s has no option %s", strings.Title((*hgcmd)[0]), optname)
 	}
 	return nil
 }
@@ -374,7 +374,7 @@ func addIntOpt(opt int, optname string, cmd string, cmdOpts interface{}, hgcmd *
 			*hgcmd = append(*hgcmd, strconv.Itoa(opt))
 		}
 	} else {
-		return fmt.Errorf("command %s has no option %s", strings.Title((*hgcmd)[0]), optname)
+		return fmt.Errorf("addIntOpt(): command %s has no option %s", strings.Title((*hgcmd)[0]), optname)
 	}
 	return nil
 }
@@ -390,7 +390,7 @@ func addStringOpt(opt string, optname string, cmd string, cmdOpts interface{}, h
 			*hgcmd = append(*hgcmd, opt)
 		}
 	} else {
-		return fmt.Errorf("command %s has no option %s", strings.Title((*hgcmd)[0]), optname)
+		return fmt.Errorf("addStringOpt(): command %s has no option %s", strings.Title((*hgcmd)[0]), optname)
 	}
 	return nil
 }
