4cee1cb3be6c93a2b7ea5fda8210ee49e1e296e7
Johan Samyn <johan.samyn@gmail.com>
2013-07-28 22:24:24 +0200

    more refining of the dcocumentation

--- doc.go
+++ doc.go
@@ -28,20 +28,22 @@ Import the package
 Start with importing the gohg package:
   import . "bitbucket.org/gohg/gohg"
 
-No real need to import as 'gohg', as the only two types needed have a name that
-starts with 'Hg': HgClient and HgCmd. (Notable exceptions are the Option types).
+No real need to import as 'gohg', as the main three types you need have a name
+that starts with 'Hg': HgClient, HgCmd and HgOption. (Notable exceptions are the
+different types that represent each option. But as their names are kind of
+typical for Hg, I considered prefixing them with 'Hg' would look too ugly.)
 
 Connecting the Mercurial Command Server
 
 All interaction with the Mercurial Command Server (Hg CS from now on) happens
 through the HgClient type, of which you have to create an instance:
 
-  hc :=  NewHgClient()
+  hgcl :=  NewHgClient()
 
-Then you can connect the Hg CS:
+Then you can connect the Hg CS as follows:
 
-  err := hc.Connect("hg", "~/myrepo", nil)
-   4                 1        2        3
+  err := hgcl.Connect("hg", "~/myrepo", nil)
+   4                   1        2        3
 
 1. The Hg executable:
 
@@ -54,7 +56,7 @@ for testing purposes for instance.
 
 The second parameter is the path to the repository you want to work on. You can
 leave it blanc to have gohg use the repository it can find for the current path
-(searching upward in the folder tree eventually).
+you are running the program in (searching upward in the folder tree eventually).
 
 3. The config for the session:
 
@@ -64,16 +66,16 @@ Though this is currently not implemented yet.
 4. The returnvalue:
 
 The HgClient.Connect() method eventually returns an error, so you can check if
-the connection succeeded, and if it is safe to go on or not.
+the connection succeeded, and if it is safe to go on.
 
-Once the work is done, you can disconnect the Hg CS. We advise to use a typical
-Go idiom for this:
+Once the work is done, you can disconnect the Hg CS. We advise to use the
+typical Go idiom for this:
 
-  err := hc.Connect("hg", "~/myrepo", nil)
+  err := hgcl.Connect("hg", "~/myrepo", nil)
   if err != nil {
       log.Fatal(err)
   }
-  defer hc.Disconnect()
+  defer hgcl.Disconnect()
   // do the real work here
 
 Config
@@ -89,7 +91,7 @@ it's good working:
 Commands
 
 Once we have a connection to a Hg CS we can do some work with the repository.
-This is done with commands, and gohg offers 3 ways for issuing them.
+This is done with commands, and gohg offers 3 ways to use them.
 
 1. The command methods of the HgClient type.
 
@@ -102,17 +104,18 @@ Each of which has its own reason of existance.
 Commands return a byte slice containing the resulting data, and eventually an
 error. But there are a few exceptions (see api docs).
 
-  log, err := hc.Log(nil, nil)       // log is a byte slice
-  err := hc.Init(nil, "~/mynewrepo") // only returns an error eventually
-  vers, err:= hc.Version()           // vers is a string of the form '2.4'
+  log, err := hgcl.Log(nil, nil)       // log is a byte slice
+  err := hgcl.Init(nil, "~/mynewrepo") // only returns an error eventually
+  vers, err:= hgcl.Version()           // vers is a string of the form '2.4'
 
-If a command fails, the returned error contains 3 elements: 1) the returncode
-by Mercurial, 2) the full command that was passed to the Hg CS, and 3) the
-eventual error message returned by Mercurial.
+If a command fails, the returned error contains 5 elements: 1) the internal
+routine where the error was trapped, 2) the name of the HgClient command that
+was run, 3) the returncode by Mercurial, 4) the full command that was passed
+to the Hg CS, and 5) the eventual error message returned by Mercurial.
 
 So the command
 
-  idinfo, err := hc.Identify([]Option{Verbose(true)}, []string{"C:\\DEV\\myrepo"})
+  idinfo, err := hgcl.Identify([]HgOption{Verbose(true)}, []string{"C:\\DEV\\myrepo"})
 
 could return something like the following in the err variable when it fails:
 
@@ -134,7 +137,7 @@ with a capital letter of course.
 
 An example (also see examples/example1.go):
 
-  log, err := hc.Log([]Option{Limit(2)}, []string("my-file"))
+  log, err := hgcl.Log([]HgOption{Limit(2)}, []string("my-file"))
   if err != nil {
       fmt.Printf(err)
       ...
@@ -142,62 +145,65 @@ An example (also see examples/example1.go):
   fmt.Printf("%s", log)
 
 Note that these methods all use the HgCmd type internally. As such they are
-convenience wrappers around that type. You could also call them a kind of
+convenience wrappers around that type. You could also consider them as a kind of
 syntactic sugar. If you just want to simply issue a command, nothing more, they
 are the way to go.
 
 The only way to obtain the commandstring sent to Hg when using these command
-methods, is by calling the HgClient.ShowLastCmd() method:
+methods, is by calling the HgClient.ShowLastCmd() method afterwards:
 
-  log, err := hc.Log([]Option{Limit(2)}, []string("my-file"))
-  fmt.Printf("%s", hc.ShowLastCmd()) // prints: log --limit 2 -v my-file
+  log, err := hgcl.Log([]HgOption{Limit(2)}, []string("my-file"))
+  fmt.Printf("%s", hc.ShowLastCmd()) // prints: log --limit 2 my-file
 
-But you have to do this before issuing any other command, as the name of the
+But you have to do this before performing any other command, as the name of the
 method already suggests.
 
 Commands - the HgCmd type
 
 Using the HgCmd type is kind of the standard way. It is a struct that you can
-instantiate for any new command, and for which you can set elements Name,
-Options and Params (see the api docs hereafter for more details). It allows for
-building the command step by step, and also to query the exact command that will
-be sent to the Hg CS.
+instantiate for any command, and for which you can set elements Name, Options
+and Params (see the api docs for more details). It allows for building the
+command step by step, and also to query the exact command that will be sent to
+the Hg CS.
 
 A pro of this method is that it allows you to obtain the exact command string
-that will be passed to Mercurial, as you would type it on the commandline. This
-could be handy for logging, or for showing feedback to the user in a GUI program.
+that will be passed to Mercurial before it is performed, by calling the
+CmdLine() method of HgCmd. This could be handy for logging, or for showing
+feedback to the user in a GUI program. (You could even call CmdLine() several
+times, and show the building of the command step by step.)
 
 An example (also see examples/example2.go):
 
-  opts := make([]Option, 2)
+  opts := make([]HgOption, 2)
   var lim Limit = 2
   opts[0] = lim
   var verb Verbose = true
   opts[1] = verb
-  hc.SetOptions(opts)
-  hc, _ := NewHgCmd("log", opts, nil, new(logOpts))
-  cmdline, _ := hc.CmdLine(hgcl)
+  cmd, _ := NewHgCmd("log", opts, nil, new(logOpts))
+  cmd.SetOptions(opts)
+  cmdline, _ := cmd.CmdLine(hgcl)
   fmt.Printf("%s\n", cmdline) // output: log --limit 2 -v
-  hc.Exec(hgcl)
+  cmd.Exec(hgcl)
 
 As you can see, this way requires some more coding.
 
-The api docs will also show you the HgCmd type has a convenient constructor for
-each command, allowing for easy and correct initialization of the new command
-type.
+The source code will also show you that the HgCmd type is indeed used as the
+underlying type for the convenience HgClient commands, in all the
+New<Hgcommand>Cmd() constructors.
 
 Commands - ExecCmd
 
 The HgClient type has an extra method ExecCmd(), allowing you to pass a fully
 custom build command to Hg. It accepts a string slice that is supposed to
-contain the complete command, as you would type it at the command line.
+contain all the elements of the complete command, as you would type it at the
+command line.
 
-It could be a convenient way for issuing commands that are not yet implemented
-in gohg, or to make use of extensions to Hg (for which gohg offers no support).
+It could be a convenient way for performing commands that are not yet
+implemented in gohg, or to make use of extensions to Hg (for which gohg offers
+no support (yet?)).
 
 An example (also see examples/example3.go):
 
-  // hgcl is a HgClient instance that has a connection to the Hg CS
   hgcmd := []string{"log", "--limit", "2"}
   result, err := hgcl.ExecCmd(hgcmd)
 
@@ -205,28 +211,27 @@ Options and Parameters
 
 Just like on the commandline, options come before parameters.
 
-  opts := []Option{Verbose(true), Limit(2)}
+  opts := []HgOption{Verbose(true), Limit(2)}
   params := []string{"mytool.go"}
-  log, err := hc.Log(opts, params)
+  log, err := hgcl.Log(opts, params)
 
 Options to commands use the same name as the long form of the Mercurial option
-they represent, but start with a capital letter (as do all exported symbols in
-Go). An options value can be of type bool, int or string. You just pass the
-value as the parameter to the option (= type conversion of the value to the
-option type). You can pass any number of options, as the elements of a slice.
-Options can occur more than once if appropriate (see the ones marked with '[+]'
-in the Mercurial help).
+they represent, but start with the necessary capital letter. An options value
+can be of type bool, int or string. You just pass the value as the parameter to
+the option (= type conversion of the value to the option type). You can pass any
+number of options, as the elements of a slice. Options can occur more than once
+if appropriate (see the ones marked with '[+]' in the Mercurial help).
 
-  log, err := hc.Log([]Option{Verbose(true)}, nil)
-  log, err := hc.Log([]Option{Limit(2)}, nil)
-  log, err := hc.Log([]Option{User("John Doe"), User("me")}, nil)
+  log, err := hgcl.Log([]HgOption{Verbose(true)}, nil)                 // bool
+  log, err := hgcl.Log([]HgOption{Limit(2)}, nil)                      // int
+  log, err := hgcl.Log([]HgOption{User("John Doe"), User("me")}, nil)  // string, repeated option
 
 Parameters are used to provide any arguments for a command that are not options.
-They are passed in as a string or a string slice, depending on the command.
+They are passed in as a string or a slice of strings, depending on the command.
 These parameters typically contain revisions, paths or filenames and so.
 
-  log, err := hc.Log(nil, []string{"myfile"})
-  heads, err := hc.Heads(nil, []string{"foobranch"})
+  log, err := hgcl.Log(nil, []string{"myfile"})
+  heads, err := hgcl.Heads(nil, []string{"foobranch"})
 
 The gohg tool only checks if the options the caller gives are valid for that
 command. It does not check if the values are valid for the combination of that
@@ -234,7 +239,7 @@ command and that option, as that is done by Mercurial. No need to implement that
 again. If an option is not valid for a command, it is silently ignored, so it is
 not passed to the Hg CS.
 
-Some options are not implemented, as they seemed not relevant for use with this
+A few options are not implemented, as they seemed not relevant for use with this
 tool (for instance: the global --color option, or the --print0 option for
 status).
 
@@ -247,19 +252,19 @@ whether to do a retry or to abort the complete application.
 
 Limitations
 
-* The following config settings are fixated in the code (at least for now):
+▪ The following config settings are fixated in the code (at least for now):
   encoding=utf-8
   ui.interactive=False
   extensions.color=!
 
-* As mentioned earlier, passing config info is not implemented yet.
+▪ As mentioned earlier, passing config info is not implemented yet.
 
-* Currently there is no support for any extensions to Mercurial.
+▪ Currently there is no support for any extensions to Mercurial.
 
-* If multiple Hg CSers are used against the same repo, it is up to Mercurial
+▪ If multiple Hg CSers are used against the same repo, it is up to Mercurial
 to handle this correctly.
 
-* Mercurial is always run in english. No internationalization yet.
+▪ Mercurial is always run in english. No internationalization yet.
 
 Issues
 
@@ -271,7 +276,7 @@ there.
 
 License
 
-Copyright 2012, The gohg Authors. All rights reserved.
+Copyright 2012-2013, The gohg Authors. All rights reserved.
 
 Use of this source code is governed by a BSD style license that can be found in
 the LICENSE.md file.
