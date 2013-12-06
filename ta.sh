0388e6d8c4347347a715cdc802e813f61847b2c4
Johan Samyn <johan.samyn@gmail.com>
2013-12-06 11:25:41 +0100

    ta.sh: make zsh compatible

--- ta.sh
+++ ta.sh
@@ -1,35 +1,44 @@
 clear
 
+shel=`ps -p $$ | tail -1 | awk '{print $NF}'`
+
+if [[ "$shel" = "bash" ]]; then
+  arg="-p "
+else # probably zsh
+  arg="\?"
+fi
+
 echo ----------------------------------------
 echo running: go test
 echo ----------------------------------------
 # %* is for passing in -v f.i.
 go test $*
-read -p "Press [enter] to continue ..."
+#read -p "Press [enter] to continue ..."
+read $arg"Press [enter] to continue ..."
 
 echo ----------------------------------------
 echo running: go run examples/stats.go
 echo ----------------------------------------
 go run examples/stats.go
-read -p "Press [enter] to continue ..."
+read $arg"Press [enter] to continue ..."
 
 echo ----------------------------------------
 echo running: go run examples/readme-test.go
 echo ----------------------------------------
 go run examples/readme-test.go
-read -p "Press [enter] to continue ..."
+read $arg"Press [enter] to continue ..."
 
 echo ----------------------------------------
 echo running: go run examples/example1.go
 echo ----------------------------------------
 go run examples/example1.go
-read -p "Press [enter] to continue ..."
+read $arg"Press [enter] to continue ..."
 
 echo ----------------------------------------
 echo running: go run examples/example2.go
 echo ----------------------------------------
 go run examples/example2.go
-read -p "Press [enter] to continue ..."
+read $arg"Press [enter] to continue ..."
 
 echo ----------------------------------------
 echo running: go run examples/example3.go
