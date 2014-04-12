653e1a544c10e2487dbbcfcb44ddb9ac41ea8d7a
Johan Samyn <johan.samyn@gmail.com>
2014-04-12 21:41:08 +0200

    servedoc.sh: added indexing

--- servedoc.sh
+++ servedoc.sh
@@ -1,2 +1,2 @@
 GOPATH=/home/johan/DEV/go
-gnome-terminal -x godoc -goroot=$GOROOT -http=localhost:6161 -tabwidth=4
+gnome-terminal -x godoc -goroot=$GOROOT -http=localhost:6161 -tabwidth=4 -index &
